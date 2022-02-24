0b92f50a5277f4185b0b1b980341d5022f1e334e
alvaroun <alvaromachadoferreira@hotmail.com>
2022-02-23 22:51:12 -0300

    update ex 2

--- js/ex2/1/main.js
+++ js/ex2/1/main.js
@@ -1,21 +1,21 @@
 // Crie um programa que pergunta quantos números o usuário deseja digitar (tanto inteiro quanto decimal) e rebeca os números 
 // e armazene esses números em um array e depois mostre a soma total deles, a média deles, mostre cada valor em uma potência de 
 // 2 e faça a raíz quadrada e cúbica de um.
-let total = parseInt(window.prompt('Quantos números você deseja digitar?'));
+let total = parseInt(prompt('Quantos números você deseja digitar?'));
 
 let arr = [];
 let sum = 0;
 let cont = 0;
 
 for(i = 0; i < total; i++) {
-    let aux = parseFloat(window.prompt(`Qual o primeiro ${i+1}° valor`));
+    let aux = parseFloat(prompt(`Qual o primeiro ${i+1}° valor`));
     arr.push(aux);
 }
 
 for(let i of arr) {
     
     sum += i;
-    window.document.write("<p>" + `<strong>Valor =</strong>
+    document.write("<p>" + `<strong>Valor =</strong>
     ${i} <br> <strong>Ao Quadrado =</strong> 
     ${Math.pow(i,2)} <br> <strong>Raiz Quadrada =</strong> 
     ${Math.sqrt(i)} <br> <strong>Raiz Cúbica =</strong> 
@@ -24,6 +24,6 @@ for(let i of arr) {
 }
 
 
-window.document.write("<p>" + "<strong>Soma Total = </strong>" + sum + "</p> ");
+document.write("<p>" + "<strong>Soma Total = </strong>" + sum + "</p> ");
 
-window.document.write("<p>" + "<strong>Média = </strong>" + sum / arr.length +"<hr></p>");
+document.write("<p>" + "<strong>Média = </strong>" + sum / arr.length +"<hr></p>");
--- js/ex2/3/main.js
+++ js/ex2/3/main.js
@@ -1,2 +1,15 @@
 // Peça ao usuário para digitar um número binário e o converta para decimal.
 
+const bin = prompt('Digite um número binário');
+const arr = bin.split('').reverse().map(x => parseInt(x));
+
+let decimal = 0
+
+arr.forEach((value, index) => {
+    if (value !== 0) {
+        decimal += 2 ** index
+    }
+})
+
+document.write("<p>" + `Valor Binário: ${bin}<br><br>Valor Decimal: ${decimal}`+ "</p> ");
+console.log(decimal);
